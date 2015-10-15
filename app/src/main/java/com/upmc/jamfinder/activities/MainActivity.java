6a3027b20721fb7398c0453d1474b7d3b9762177
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-10-15 18:12:34 +0200

    Map fonctionnelle

--- app/src/main/java/com/upmc/jamfinder/activities/MainActivity.java
+++ app/src/main/java/com/upmc/jamfinder/activities/MainActivity.java
@@ -1,11 +1,6 @@
 package com.upmc.jamfinder.activities;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
@@ -16,16 +11,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -34,7 +30,7 @@ import com.upmc.jamfinder.customLayouts.DrawerPanelListAdapter;
 
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private String TAG = "MAIN_ACTIVITY";
 
@@ -51,16 +47,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private String[] mDrawerMenuEntries;
 
     private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buildGoogleApiClient();
-        mGoogleApiClient.connect();
-
 
         mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.main_map);
         mMapFragment.getMapAsync(this);
@@ -77,14 +70,68 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mDrawerList = (ListView) findViewById(R.id.main_drawer_list);
         mDrawerList.setAdapter(new DrawerPanelListAdapter(this, mDrawerMenuEntries));
 
+        buildGoogleApiClient();
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+
     }
 
-    protected synchronized void buildGoogleApiClient() {
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            stopLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    private void updateUI(){
+        LatLng loc = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        //mMarker = mMap.addMarker(new MarkerOptions().position(loc));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 14.0f));
+
+    }
+
+    private synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+        createLocationRequest();
+    }
+
+
+    private void createLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10 * 1000);
+        mLocationRequest.setFastestInterval(5 * 1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void stopLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
     @Override
@@ -115,20 +162,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void setUpMap() {
-        if (mMap == null) {
-            Log.d(TAG, "Map NULL");
-            return;
-        }
-
         mMap.setMyLocationEnabled(true);
 
-        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-            @Override
-            public void onMyLocationChange(Location location) {
-                Log.d(TAG, "Postion changee");
-                mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())));
-            }
-        });
     }
 
     @Override
@@ -141,12 +176,18 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onConnected(Bundle bundle) {
-
+        if(mCurrentLocation == null){
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(mCurrentLocation != null) {
+                updateUI();
+            }
+        }
+        startLocationUpdates();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -155,4 +196,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         Log.e(TAG, connectionResult.toString());
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        Toast.makeText(this, "Location changée",
+                Toast.LENGTH_SHORT).show();
+        updateUI();
+    }
 }
