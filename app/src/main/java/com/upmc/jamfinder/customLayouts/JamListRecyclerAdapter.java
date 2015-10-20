602356d2fb1c9d8332c9fe0025d2e902e4def9b5
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-10-20 20:28:35 +0200

    Maps dans Cards

--- app/src/main/java/com/upmc/jamfinder/customLayouts/JamListRecyclerAdapter.java
+++ app/src/main/java/com/upmc/jamfinder/customLayouts/JamListRecyclerAdapter.java
@@ -8,6 +8,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.upmc.jamfinder.R;
 import com.upmc.jamfinder.model.Jam;
 
@@ -16,12 +21,15 @@ import java.util.ArrayList;
 /**
  * Created by maxime on 20/10/15.
  */
-public class JamListRecyclerAdapter extends RecyclerView.Adapter<JamListRecyclerAdapter.ViewHolder> {
+public class JamListRecyclerAdapter extends RecyclerView.Adapter<JamListRecyclerAdapter.ViewHolder> implements OnMapReadyCallback {
 
 
     private ArrayList<Jam> mJamList;
 
+
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        MapView mMapView;
         TextView mTextView;
         Button mEditButton;
         Button mDeleteButton;
@@ -31,6 +39,7 @@ public class JamListRecyclerAdapter extends RecyclerView.Adapter<JamListRecycler
             mTextView = (TextView) v.findViewById(R.id.recycler_card_jam_name);
             mEditButton = (Button) v.findViewById(R.id.recycler_card_jam_edit_button);
             mDeleteButton = (Button) v.findViewById(R.id.recycler_card_jam_delete_button);
+            mMapView = (MapView) v.findViewById(R.id.recycler_card_jam_map_view);
         }
     }
 
@@ -49,11 +58,24 @@ public class JamListRecyclerAdapter extends RecyclerView.Adapter<JamListRecycler
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.mTextView.setText(mJamList.get(position).getName());
+
+        holder.mMapView.onCreate(null);
+        holder.mMapView.getMapAsync(this);
+
+        holder.mMapView.getMap()
+                .addMarker(new MarkerOptions().position(mJamList.get(position).getLocation()));
+        holder.mMapView.getMap()
+                .animateCamera(CameraUpdateFactory.newLatLngZoom(mJamList.get(position).getLocation(), 13.0f));
     }
 
     @Override
     public int getItemCount() {
         return mJamList.size();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 
 }
